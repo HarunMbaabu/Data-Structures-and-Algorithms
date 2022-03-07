6e6a410d4f109d6c3706c2c8296dc221f1b0ba50
harunmwenda <hmwenda@mfsafrica.com>
2022-03-07 16:25:40 +0300

    first uploads

---
+++ Python/LengOfTheLastWord.py
@@ -0,0 +1,10 @@
+def length_of_last_word(s):
+        words = s.split()
+        if len(words) == 0:
+            return 0
+        return len(words[-1])
+
+print(length_of_last_word("Python Exercises"))
+print(length_of_last_word("Python"))
+print(length_of_last_word(""))
+print(length_of_last_word("  "))
